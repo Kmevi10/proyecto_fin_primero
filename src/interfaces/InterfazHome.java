e10a7ae93771f085d5b22e84a9da18a313816315
Carlos <carlosmesaviera@gmail.com>
2019-05-16 20:41:43 +0200

    Merge branch 'feature/ficheros' into develop

--- src/interfaces/InterfazHome.java
+++ src/interfaces/InterfazHome.java
@@ -6,6 +6,7 @@ import Funciones.Funciones;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.sql.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
